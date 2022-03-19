164fb4eadbb97336894d2f33f6cda999c717170f
Xavier <xavier@bird.eu>
2022-03-19 20:55:05 +0100

    added horizontal movements

--- Block/Snowflake.php
+++ Block/Snowflake.php
@@ -5,27 +5,36 @@ namespace Opengento\Snowflake\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Opengento\Snowflake\Model\Config\Snowflake as SnowflakeConfig;
 
 class Snowflake extends Template
 {
-    const SNOWFLAKE_CHAR = 'snowflake/general/icon';
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    protected $config;
+    protected ScopeConfigInterface $snowflakeConfig;
+    protected SnowflakeConfig $config;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
+        SnowflakeConfig $snowflakeConfig,
         Template\Context $context,
         array $data = []
     ) {
+        $this->SnowflakeConfig = $snowflakeConfig;
         $this->scopeConfig = $scopeConfig;
         parent::__construct($context, $data);
     }
 
     public function getSnowflakeChar() : string
     {
-        return $this->scopeConfig->getValue(self::SNOWFLAKE_CHAR, ScopeConfigInterface::SCOPE_TYPE_DEFAULT) ?? '';
+        return $this->SnowflakeConfig->getSnowflakeChar();
+    }
+
+    public function getSnowflakeVSpeed() : string
+    {
+        return $this->SnowflakeConfig->getSnowflakeVSpeed();
+    }
+
+    public function getSnowflakeHSpeed() : string
+    {
+        return $this->SnowflakeConfig->getSnowflakeHSpeed();
     }
 }
---
+++ Model/Config/Snowflake.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © OpenGento, All rights reserved.
+ * See LICENSE bundled with this library for license details.
+ */
+declare(strict_types=1);
+
+namespace Opengento\Snowflake\Model\Config;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Snowflake
+{
+    private const CONFIG_PATH_SNOWFLAKE_ENABLE = 'opengento/snowflake/enable';
+    private const CONFIG_PATH_SNOWFLAKE_CHAR  = 'snowflake/general/icon';
+    private const CONFIG_PATH_SNOWFLAKE_V_SPEED  = 'snowflake/general/vspeed';
+    private const CONFIG_PATH_SNOWFLAKE_H_SPEED  = 'snowflake/general/hspeed';
+
+    private ScopeConfigInterface $scopeConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function isEnabled(?int $scopeId = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_SNOWFLAKE_ENABLE, ScopeInterface::SCOPE_STORE, $scopeId);
+    }
+
+    public function getSnowflakeChar(?int $scopeId = null): string
+    {
+        return $this->scopeConfig->getValue(self::CONFIG_PATH_SNOWFLAKE_CHAR, ScopeInterface::SCOPE_STORE, $scopeId) ?? '';
+    }
+
+    public function getSnowflakeVSpeed(?int $scopeId = null): string
+    {
+        return $this->scopeConfig->getValue(self::CONFIG_PATH_SNOWFLAKE_V_SPEED, ScopeInterface::SCOPE_STORE, $scopeId) ?? '';
+    }
+
+    public function getSnowflakeHSpeed(?int $scopeId = null): string
+    {
+        return $this->scopeConfig->getValue(self::CONFIG_PATH_SNOWFLAKE_H_SPEED, ScopeInterface::SCOPE_STORE, $scopeId) ?? '';
+    }
+}
